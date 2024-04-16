f2d235cafb3471ef42526bdabd319de373e92779
ISAWASHUN <syun20000616@icloud.com>
2024-04-16 22:14:27 +0900

    ステップ4の終了 && エラーメッセージの記入

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -20,6 +20,8 @@ struct Token {
   char *str;      // Token string
 };
 
+char *user_input;
+
 // Current token
 Token *token;
 
@@ -28,6 +30,18 @@ void error(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
   vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, "");
+  fprintf(stderr, "^ ");
   fprintf(stderr, "\n");
   exit(1);
 }
@@ -43,14 +57,14 @@ bool consume(char op) {
 // Ensure that the current token is `op`.
 void expect(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("expected '%c'", op);
+    error_at(token->str, "expected '%c'", op);
   token = token->next;
 }
 
 // Ensure that the current token is TK_NUM.
 int expect_number() {
   if (token->kind != TK_NUM)
-    error("expected a number");
+    error_at(token->str, "expected a number");
   int val = token->val;
   token = token->next;
   return val;
@@ -70,7 +84,8 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
 }
 
 // Tokenize `p` and returns new tokens.
-Token *tokenize(char *p) {
+Token *tokenize() {
+  char *p = user_input;
   Token head;
   head.next = NULL;
   Token *cur = &head;
@@ -95,7 +110,7 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    error("invalid token");
+    error_at(p, "expected a number");
   }
 
   new_token(TK_EOF, cur, p);
@@ -108,6 +123,7 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  user_input = argv[1];
   token = tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
