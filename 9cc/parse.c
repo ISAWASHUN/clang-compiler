0cabe23add54cc96166f75fde07cf1f108499ba1
ISAWASHUN <syun20000616@icloud.com>
2024-04-22 20:09:44 +0900

    ステップ13の終了

--- 9cc/parse.c
+++ 9cc/parse.c
@@ -4,6 +4,7 @@
 // accumulated to this list.
 Obj *locals;
 
+static Node *compound_stmt(Token **rest, Token *tok);
 static Node *expr(Token **rest, Token *tok);
 static Node *expr_stmt(Token **rest, Token *tok);
 static Node *assign(Token **rest, Token *tok);
@@ -61,9 +62,33 @@ static Obj *new_lvar(char *name) {
   return var;
 }
 
-// stmt = expr-stmt
+// stmt = "return" expr ";"
+//      | "{" compound-stmt
+//      | expr-stmt
 static Node *stmt(Token **rest, Token *tok) {
+  if (equal(tok, "return")) {
+    Node *node = new_unary(ND_RETURN, expr(&tok, tok->next));
+    *rest = skip(tok, ";");
+    return node;
+  }
+
+  if (equal(tok, "{"))
+    return compound_stmt(rest, tok->next);
+
   return expr_stmt(rest, tok);
+}
+
+// compound-stmt = stmt* "}"
+static Node *compound_stmt(Token **rest, Token *tok) {
+  Node head = {};
+  Node *cur = &head;
+  while (!equal(tok, "}"))
+    cur = cur->next = stmt(&tok, tok);
+
+  Node *node = new_node(ND_BLOCK);
+  node->body = head.next;
+  *rest = tok->next;
+  return node;
 }
 
 // expr-stmt = expr ";"
@@ -191,12 +216,6 @@ static Node *unary(Token **rest, Token *tok) {
 
 // primary = "(" expr ")" | ident | num
 static Node *primary(Token **rest, Token *tok) {
-  if (equal(tok, "return")) {
-    Node *node = new_unary(ND_RETURN, expr(&tok, tok->next));
-    *rest = skip(tok, ";");
-    return node;
-  }
-  
   if (equal(tok, "(")) {
     Node *node = expr(&tok, tok->next);
     *rest = skip(tok, ")");
@@ -222,14 +241,10 @@ static Node *primary(Token **rest, Token *tok) {
 
 // program = stmt*
 Function *parse(Token *tok) {
-  Node head = {};
-  Node *cur = &head;
-
-  while (tok->kind != TK_EOF)
-    cur = cur->next = stmt(&tok, tok);
+  tok = skip(tok, "{");
 
   Function *prog = calloc(1, sizeof(Function));
-  prog->body = head.next;
+  prog->body = compound_stmt(&tok, tok);
   prog->locals = locals;
   return prog;
 }
